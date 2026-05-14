b61e257d9ebe50e5d21cc1ad140b2a0fcb6b9dd2
benete21 <benyubun@gmail.com>
2026-05-14 14:59:02 +0200

    finishing pickup object

--- Feed the Fiend/Assets/Scripts/Chef_Controls.cs
+++ Feed the Fiend/Assets/Scripts/Chef_Controls.cs
@@ -11,7 +11,12 @@ public class Chef_Controls : MonoBehaviour
     private Vector2 moveInput;
 
     [Header("Pickup")]
+    [SerializeField] Transform hold;
+    private GameObject heldObj;
+    private Rigidbody heldrb;
+
     [SerializeField] private float pickupRange = 3f;
+    [SerializeField] private float pickupForce = 150.0f;
 
     private void Update()
     {
@@ -31,6 +36,59 @@ public class Chef_Controls : MonoBehaviour
     public void OnLook(InputAction.CallbackContext context)
     {
         lookInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnPickUp(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        if (heldObj == null)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickupRange))
+            {
+                PickUp(hit.transform.gameObject);
+            }
+        }
+        else
+        {
+            DropObj();
+        }
+        if (heldObj != null)
+        {
+            MoveObj();
+        }
+    }
+
+    public void PickUp(GameObject pick)
+    {
+        if (pick.GetComponent<Rigidbody>())
+        {
+            heldrb = pick.GetComponent<Rigidbody>();
+            heldrb.useGravity = false;
+            heldrb.drag = 10;
+            heldrb.constraints = RigidbodyConstraints.FreezeRotation;
+
+            heldrb.transform.parent = hold;
+            heldObj = pick;
+
+        }
+    }
+    public void DropObj()
+    {
+        heldrb.useGravity = true;
+        heldrb.drag = 1;
+        heldrb.constraints = RigidbodyConstraints.FreezeRotation;
+
+        heldrb.transform.parent = null;
+        heldObj = null;
+    }
+    public void MoveObj()
+    {
+        if (Vector3.Distance(heldObj.transform.position, hold.position) > 0.1f)
+        {
+            Vector3 moveDir = (hold.position - heldObj.transform.position).normalized;
+            heldrb.AddForce(moveDir * pickupForce);
+        }
     }
 
 }
