90e5895f19b8ddaea11f3ed26ef725b2a6efdc92
benete21 <benyubun@gmail.com>
2026-05-14 15:41:22 +0200

    instaintiate scpeit

--- Feed the Fiend/Assets/Scripts/Ingredient_Spawner.cs
+++ Feed the Fiend/Assets/Scripts/Ingredient_Spawner.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 public class Ingredient_Spawner : MonoBehaviour
 {
     public GameObject [] typeFood = new GameObject[6];
+    public GameObject objectToSpawn;
+    public Transform player;
+    public float spawnDistance = 5.0f;
+    private bool hasSpawned = false;
 
-    public void GetFood()
+    void Update()
     {
-
+        if (!hasSpawned && Vector3.Distance(transform.position, player.position) < spawnDistance)
+        {
+            Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+            hasSpawned = true;
+        }
     }
-
 }
+
