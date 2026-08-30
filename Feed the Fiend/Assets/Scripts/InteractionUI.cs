06819720eb91d8cd239870e81fad6c0c6f7ac8f5
benete21 <benyubun@gmail.com>
2026-08-30 22:12:36 +0200

    adding u=UI interface

---
+++ Feed the Fiend/Assets/Scripts/InteractionUI.cs
@@ -0,0 +1,26 @@
+using TMPro;
+using UnityEngine;
+
+public class InteractionUI : MonoBehaviour
+{
+
+    [SerializeField] private GameObject promptObject;
+    [SerializeField] private TMP_Text promptText;
+
+    private void Awake()
+    {
+        Hide();
+    }
+
+    public void Show(string message)
+    {
+        promptText.text = message;
+        promptObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        promptObject.SetActive(false);
+    }
+}
+
